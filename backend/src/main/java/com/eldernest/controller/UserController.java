14d6565ed0b10a3a0064c836d377201354d06c3f
sajan-2003 <sajanchamika@gmail.com>
2026-07-25 22:29:31 +0530

    Add user CRUD REST endpoints

--- backend/src/main/java/com/eldernest/controller/UserController.java
+++ backend/src/main/java/com/eldernest/controller/UserController.java
@@ -1,4 +1,41 @@
 package com.eldernest.controller;
+import com.eldernest.model.User;
+import com.eldernest.service.UserService;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
 
+@RestController
+@RequestMapping("/api/v1/users")
 public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+    @GetMapping("/{id}")
+    public User getUserById(@PathVariable Long id) {
+        return userService.getUserById(id);
+    }
+    @PostMapping
+    public User addUser(@RequestBody User user) {
+        return userService.addUser(user);
+    }
+
+    @DeleteMapping("/{id}")
+    public boolean deleteUser(@PathVariable Long id){
+        return userService.deleteUser(id);
+    }
+
+    @PutMapping("/{id}")
+    public User updateUserById(@PathVariable Long id,@RequestBody User user){
+        return userService.updateUser(id,user);
+    }
+
+
+
 }
